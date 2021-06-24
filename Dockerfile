86a09aa6b9c8662510f37c1dd7907c3d7edb42d5
Jayant Goel <jgoel92@gmail.com>
2021-06-24 15:32:16 +0530

    Initial Commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM python:3.9
+
 LABEL maintainer="Jayant Goel"
+
 COPY . /app
 WORKDIR /app
+
 RUN pip install -r requirements.txt
+
 CMD ["python" , "app.py"]
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return 'Hello World from Docker!'
-
 app.run(host='0.0.0.0', port=5000)
